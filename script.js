06a7d155a3c69553964c1e7dad49a3ce701995f5
Linda Aurora Perez <lin.auro.ch@gmail.com>
2022-09-26 11:19:59 -0500

    feat: uso de script

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Saoko Papi Saokooo");
+document.getElementById("generation").style.color - "grey" 
+function colorTitulo(){
+    console.log("Pulso el boton de Generation");
+    document.getElementById("generation").style.color = "red";
+}
